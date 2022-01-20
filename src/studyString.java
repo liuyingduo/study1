e31b33c40c8c663cfe2a26f66bd34931d511301f
liuyingduo <A2711985388@163.com>
2022-01-20 22:08:12 +0800

    Initial commit

---
+++ src/studyString.java
@@ -0,0 +1,11 @@
+public class studyString {
+    public static void main(String[] args) {
+        // 1、第一个例子
+//        char[] helloArray = {'r','u','n','o','o','o'};
+//        String helloString = new String(helloArray);
+//        System.out.println(helloString);
+        String site = "www.runoob.com";
+        int len = site.length();
+        System.out.println("菜鸟教程网址长度："+len);
+    }
+}
